b48413a4e1cd073c90e7f86981891a16d2997d4f
Conrad Heidebrecht <conrad.heidebrecht@gmail.com>
2020-05-31 20:20:27 -0400

    fixed routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,7 +3,7 @@ import 'package:flattenthecarboncurve/screens/home_screen.dart';
 import 'package:flattenthecarboncurve/screens/welcome_screen.dart';
 
 final routes = {
-  '/': (context) => CalculatorScreen(),
+  '/': (context) => WelcomeScreen(),
   '/calculator': (context) => CalculatorScreen(),
   '/home': (context) => HomeScreen(),
 };
